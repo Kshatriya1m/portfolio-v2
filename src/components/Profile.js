a80f6be0e19a294c92cec9abee7edb1d6328fd4e
Mayank Chauhan <chauhanmayank.009.m@gmail.com>
2026-08-08 12:00:24 +0530

    Update download link for resume in Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -168,7 +168,7 @@ export default function Profile({ mode, url, name, email }) {
       </div>
 
       <div className="flex justify-center mt-14">
-        
+        <a
           href="/mayank_full_stack_web_developer.pdf"
           download="mayank_full_stack_web_developer.pdf"
           className="px-6 py-3 rounded-lg text-lg font-semibold shadow-lg transition"
