4699be239f74b1a6b53c39b9aed8d5426468c0d8
Mayank Chauhan <chauhanmayank.009.m@gmail.com>
2026-08-09 14:28:47 +0530

    Implement dynamic background color based on mode
    Added useEffect to change background color based on mode.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-
 // Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -18,14 +16,12 @@ const tel = "+917465942719";
 function App() {
   const [mode, setMode] = useState("dark");
 
+  useEffect(() => {
+    document.body.style.backgroundColor = mode === "dark" ? "#08080F" : "#FAFAFC";
+  }, [mode]);
+
   const togglemode = () => {
-    if (mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "#003049";
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "#e5e5e5";
-    }
+    setMode(mode === "light" ? "dark" : "light");
   };
 
   return (
@@ -33,7 +29,6 @@ function App() {
       {/* Header + Navbar Stay Same on All Pages */}
       <Header email={email} tel={tel} mode={mode} togglemode={togglemode} />
       <Navbar title={name} email={email} mode={mode} />
-
       {/* Router Pages */}
       <Routes>
         <Route
@@ -42,7 +37,6 @@ function App() {
             <Home imgurl={imgurl} name={name} email={email} mode={mode} />
           }
         />
-
         <Route path="/about" element={<About mode={mode} />} />
         <Route path="/projects" element={<Projects mode={mode} />} />
         <Route path="/experience" element={<Experience mode={mode} />} />
